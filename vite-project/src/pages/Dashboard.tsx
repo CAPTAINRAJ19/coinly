f7c811769e76cdb0455a5616d887f64b00626fcc
CAPTAINRAJ_19 <dhruvrajsingh.1019@gmail.com>
2025-06-28 10:30:08 +0530

    fixing errors

--- vite-project/src/pages/Dashboard.tsx
+++ vite-project/src/pages/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import  { useState, useEffect } from 'react';
 import { auth } from '../firebase'; // Updated path - remove '/config'
 import type { User } from 'firebase/auth'; // Type-only import
 
--- vite-project/src/pages/Home.tsx
+++ vite-project/src/pages/Home.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import CoinlyHero from '../components/HeroSection'
 
 function Home() {
